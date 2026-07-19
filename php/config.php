607366ab7287c9246b3d72bb69b47e9f7a18138b
srija0731 <damasrija07@gmail.com>
2026-07-19 23:05:53 +0530

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "medical_reminder";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+?>
